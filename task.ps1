d11984c7c3c9dd80098d9f6ec80259ff7e96122e
zakky555 <ake.zakky@gmail.com>
2017-10-02 22:46:35 +0700

    Add list and add function

--- task.ps1
+++ task.ps1
@@ -1,13 +1,24 @@
 param ( 
-      $command 
+      $command , $param 
 ) 
 
 function list { 
-    Write-Output "Select List Function" 
+    [xml]$xml = Get-Content /Users/zakky555/Desktop/powershell-learning/public/todo.list.xml
+#    Write-Output "Select List Function"
+    foreach ($task in $xml.todo.task){
+          Write-Output $task
+    }
 } 
 
-function add { 
-    Write-Output "Select Add Function" 
+function add {
+    [xml]$xml = Get-Content /Users/zakky555/Desktop/powershell-learning/public/todo.list.xml
+
+#    Write-Output "Select Add Function" 
+    $newnode  = $xml.CreateElement('task')
+    $newNode.InnerText = $param 
+    $xml.todo.AppendChild($newNode)
+    $xml.Save('/Users/zakky555/Desktop/powershell-learning/public/todo.list.xml')
+    list
 } 
 
 function done { 
